db219c588b186f231868e85e9eff21f2f3296af7
Jesse Brooks <brooks.jesse83@gmail.com>
2022-02-08 16:29:13 -0800

    bugs

--- gameZ-starter/gameZ-starter-main/react-app/src/components/ShoppingCartPreview/ShoppingCartPreview.js
+++ gameZ-starter/gameZ-starter-main/react-app/src/components/ShoppingCartPreview/ShoppingCartPreview.js
@@ -3,18 +3,24 @@ import { useSelector } from 'react-redux'
 
 function ShoppingCartPreview(){
     const cartItems = useSelector(state => state?.shoppingCart?.cartItems)
-    // const products = useSelector(state => state?.)
-
+    const products = useSelector(state => state?.product?.entries)
+    let price;
     return (
         <div className='shopping-cart-preview'>
             <h1>Shopping Cart Preview Component</h1>
-            {/* {cartItems.map(item => {
-                <li>
-                    Product Name: {item.product}
-                </li>
-            })} */}
+            {cartItems.cart_items?.map(item => {
+                const currProduct = products.find(product => item.product_id === product.id);
+                price += currProduct.price;
+                return (<li>
+                    Product Name: {currProduct.name}
+                    Quantity: {item.quantity}
+                    Price: {currProduct.price}
+                </li>)
+            })}
+            <li>
+                Cart Total:
+            </li>
         </div>
-
     )
 };
 

13cda891e1c0acfe429bf02245d47e1ab97c3d49
Xander G <90068726+gitxandalf@users.noreply.github.com>
2022-02-08 17:31:48 -0800

    implemented product DELETION (#14)

--- gameZ-starter/gameZ-starter-main/app/models/user.py
+++ gameZ-starter/gameZ-starter-main/app/models/user.py
@@ -14,11 +14,11 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
     product = db.relationship(
-        "Product", back_populates="user", cascade="all, delete-orphan")
+        "Product", back_populates="user")
     review = db.relationship(
-        "Review", back_populates="user", cascade="all, delete-orphan")
+        "Review", back_populates="user")
     shopping_cart = db.relationship(
-        "ShoppingCart", back_populates="user", cascade="all, delete-orphan")
+        "ShoppingCart", back_populates="user")
 
 
     @property

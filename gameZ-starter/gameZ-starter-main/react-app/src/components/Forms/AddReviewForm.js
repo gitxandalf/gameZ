c86039609a6b1b201456e6e525b28bd1e881246f
Xander G <90068726+gitxandalf@users.noreply.github.com>
2022-02-09 13:27:19 -0800

    implemented proper data parsing to front end - changed data sent to state (#20)

--- gameZ-starter/gameZ-starter-main/react-app/src/components/Forms/AddReviewForm.js
+++ gameZ-starter/gameZ-starter-main/react-app/src/components/Forms/AddReviewForm.js
@@ -9,83 +9,80 @@ import { postReview } from '../../store/review';
 
 
 const AddReviewForm = ({ productId }) => {
-   const history = useHistory()
-   const dispatch = useDispatch()
+    const history = useHistory()
+    const dispatch = useDispatch()
 
-   const[errors, setErrors] = useState([])
-   const[title, setTitle] = useState('')
-   const[content, setContent] = useState('')
-   const user = useSelector(state => state.session.user)
+    const [errors, setErrors] = useState([])
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    const user = useSelector(state => state.session.user)
 
-//     useEffect(() => {
-//         const errors = []
+    //     useEffect(() => {
+    //         const errors = []
 
-//         // if (!categoryId) errors.push("Please select a category")
-//         // if (name?.length > 50 | | name?.length <= 0) errors.push("Name must be less 50 characters")
-//         // if (imageUrl?.length > 255 | | imageUrl?.length <= 0) errors.push("Image Url is must be less 255 characters")
-//         // if (!price | | typeof price == = "number") errors.push("Please provide a valid price")
-//         setErrors(errors)
-//     }, [title, content])
+    //         // if (!categoryId) errors.push("Please select a category")
+    //         // if (name?.length > 50 | | name?.length <= 0) errors.push("Name must be less 50 characters")
+    //         // if (imageUrl?.length > 255 | | imageUrl?.length <= 0) errors.push("Image Url is must be less 255 characters")
+    //         // if (!price | | typeof price == = "number") errors.push("Please provide a valid price")
+    //         setErrors(errors)
+    //     }, [title, content])
 
-   let review;
-   const onSubmit = async(e) => {
-       e.preventDefault()
-       if(user) {
-           review = await dispatch(postReview({productId, title, content, userId: user.id}))
-     }
-       if(review) {
+    let review;
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        if (user) {
+            review = await dispatch(postReview({ productId, title, content, userId: user.id }))
+        }
+        if (review) {
 
-           history.push(`/products/${productId}`)
-     }
-   }
-
-
-   const updateTitle = (e) => {
-       setTitle(e.target.value)
-   }
-
-   const updateContent = (e) => {
-       setContent(e.target.value)
-   }
+            history.push(`/products/${productId}`)
+        }
+    }
 
 
-     return(
-        <div id ="reivew-product-div">
-            <form className = "review-product-form" onSubmit = { onSubmit }>
+    const updateTitle = (e) => {
+        setTitle(e.target.value)
+    }
+
+    const updateContent = (e) => {
+        setContent(e.target.value)
+    }
+
+
+    return (
+        <div id="reivew-product-div">
+            <form className="review-product-form" onSubmit={onSubmit}>
                 <div>
                     {errors && errors?.map((error, ind) => (
-                    <div key = {ind}>{error}</div>
+                        <div key={ind}>{error}</div>
                     ))}
                 </div>
                 < h2 > Write a review </h2>
                 <div>
-                <label> Title </label>
-                <input
+                    <label> Title </label>
+                    <input
                         type='text'
                         name='title'
                         onChange={updateTitle}
                         value={title}
-                ></input>
+                    ></input>
                 </div>
                 <div>
-                <label> Review </label>
-                <textarea
+                    <label> Review </label>
+                    <textarea
+                        className='text-area'
                         type='text'
                         name='content'
                         required
                         disabled={errors.length > 0}
                         onChange={updateContent}
                         value={content}
-                ></textarea>
+                    ></textarea>
                 </div>
-                <button type = 'submit'> Submit </button>
+                <button type='submit'> Submit </button>
             </form>
         </div>
     )
 }
 
- export default AddReviewForm;
-
-
-  
-
+export default AddReviewForm;

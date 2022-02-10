1cd2d27782902f9b24211951a281cae2004ef6f9
Eric Cortez <80999718+Eric-Cortez@users.noreply.github.com>
2022-02-09 16:40:52 -0800

    Category page css (#22)
    * category detail page css flex
    
    * drop shadow on products listed
    
    * fixed a tags in footer
    
    * css

--- gameZ-starter/gameZ-starter-main/react-app/src/components/Footer/index.js
+++ gameZ-starter/gameZ-starter-main/react-app/src/components/Footer/index.js
@@ -1,16 +1,15 @@
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 import "./Footer.css"
 
 const Footer = () => {
 
     return (
         <footer>
-            <Link className="footer-links" to="https://www.linkedin.com/in/suhayl-khan-48601a193/">Suhayl Khan</Link>
-            <Link className="footer-links" to="https://www.linkedin.com/in/jesse-brooks-8a6718229/">Jesse Brooks</Link>
-            <Link className="footer-links" to="https://www.linkedin.com/in/alexander-gangemi/">Alexander Gangemi </Link>
-            <Link className="footer-links" to="https://www.linkedin.com/in/eric-cortez-0101/">Eric Cortez</Link>
+            <a className="footer-links" href="https://www.linkedin.com/in/suhayl-khan-48601a193/">Suhayl Khan</a>
+            <a className="footer-links" href="https://www.linkedin.com/in/jesse-brooks-8a6718229/">Jesse Brooks</a>
+            <a className="footer-links" href="https://www.linkedin.com/in/alexander-gangemi/">Alexander Gangemi </a>
+            <a className="footer-links" href="https://www.linkedin.com/in/eric-cortez-0101/">Eric Cortez</a>
         </footer>
     );
 }

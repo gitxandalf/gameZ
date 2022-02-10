1f7c502545c7d182516390016f538550fae35624
Eric-Cortez <ecrcortez@gmail.com>
2022-02-09 16:38:03 -0800

    css

--- gameZ-starter/gameZ-starter-main/react-app/src/components/CategoryDetail/index.js
+++ gameZ-starter/gameZ-starter-main/react-app/src/components/CategoryDetail/index.js
@@ -27,11 +27,16 @@ function CategoryDetail() {
                 <div id="category-title-div">
                     <h1 id="category-h1"> {allCategories[`${categoryId - 1}`]?.name} Games</h1>
                 </div>
+                <h2 id="slogan-cat-pg">Find something you love</h2>
                 <div className='product-links'>{allProducts?.filter(product => product?.category_id === parseInt(categoryId)).map((product) => (
-                    <div id="each-product-category"key={product?.id}>
+                    <div id="each-product-category" key={product?.id}>
                         <Link id="image-link-a" key={product?.id} to={`/products/${product?.id}`}><img key={product?.id} className='image-link' src={product?.image_url}></img></Link>
                         <div id="category-product-info">
-                            <Link key={product?.id} to={`/products/${product?.id}`}> <br /> Title: {product?.name} <br /> Description: {product?.description} <br /> Price: {`$${product?.price}`}</Link>
+                            <Link id="info-link-a" key={product?.id} to={`/products/${product?.id}`}>
+                                <p className="product-info-cat title-product-cat" key={product?.id}> Title: {product?.name}</p>
+                                <p className="product-info-cat description-product-cat" key={product?.id}>Description: {product?.description} </p>
+                                <p className="product-info-cat price-product-cat" key={product?.id}>{`$${product?.price}`}</p>
+                            </Link>
                         </div>
                     </div>
                 ))}</div>

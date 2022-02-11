4c6be3983511d7c279e339ecaa5871ba8ad874b7
Eric-Cortez <ecrcortez@gmail.com>
2022-02-10 19:42:13 -0800

    formated reviews

--- gameZ-starter/gameZ-starter-main/react-app/src/components/ProductDetail/index.js
+++ gameZ-starter/gameZ-starter-main/react-app/src/components/ProductDetail/index.js
@@ -78,43 +78,61 @@ function ProductDetail({ products }) {
     }
 
     return (
-        <div id="product-detail-div">
-            <h1>Game Details</h1>
-            <img id="product-image" src={`${product?.image_url}`} alt='Product-Details' />
-            <h2>Name: {product?.name}</h2>
-            <p>Developer: {stateUsers[product?.user_id]?.username}</p>
-            <p>Category: {category[product?.category_id - 1]?.name}</p>
-            <p>Price: {`$${product?.price}`}</p>
-            <p>Description: {product?.description}</p>
-            {!(product?.user_id === user?.id) &&
-                <div>
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            className='quantity-input'
-                            type='number'
-                            value={itemQuantity}
-                            onChange={(e) => setItemQuantity(e.target.value)}></input>
-                        <button type='submit'>Add to cart</button>
-                    </form>
-                </div>}
-            <NavLink hidden={user?.id === product?.user_id ? false : true} to={`/products/${product?.id}/edit-product`} value={product?.id} className="edit">Edit</NavLink>
-            <button hidden={user?.id === product?.user_id ? false : true} className="delete" value={product?.id} onClick={handleDelete} type="submit">Delete</button>
-            <h2>Review</h2>
-            {allReviews && allReviews?.filter(review => review?.product_id === parseInt(productId)).map(review => (
-                <div>
-                    <p>Review Title: {review?.title}</p>
-                    <p>Review Content: {review?.content}</p>
-                    {user &&
-                        <>
-                            <NavLink hidden={review?.user_id !== user.id} to={`/reviews/${review?.id}/edit-reviews`} value={review?.id} className="edit">Edit</NavLink>
-                            <button hidden={review?.user_id !== user.id} className="delete" value={review?.id} onClick={handleReviewDelete} type="submit">Delete</button>
-                        </>
-                    }
-                </div>
-            ))}
+        <div >
+            <div id="product-detail-div">
 
-            {user && !(product?.user_id === user?.id) &&
-                <AddReviewForm productId={productId} />}
+                <div id="left-detail-div">    
+                    <img id="product-image" src={`${product?.image_url}`} alt='Product-Details' />
+
+                    <h2 className='review-label'>{allReviews.length} reviews</h2>
+                    <div id="product-review-heading">
+                        <h4 id="product-review-h4">Buyers are raving! </h4>
+                        <p id="product-review-p"> Multiple people gave positive reviews to this shop in the past 7 days.</p>
+                    </div>
+                    <h5 id="product-review-h5">Reviews for this item</h5>
+                    <div id="line-div-gray"></div>
+
+                    {allReviews && allReviews?.filter(review => review?.product_id === parseInt(productId)).map(review => (
+                        <div id="review-content-div">
+                            <p>{stateUsers[review.user_id].username}</p>
+                            <img src={`${stateUsers[review.user_id]?.image_url}`} alt='user' />
+                            <p>Title: {review?.title}</p>
+                            <p>Content: {review?.content}</p>
+                            {user &&
+                                <>
+                                    <NavLink hidden={review?.user_id !== user.id} to={`/reviews/${review?.id}/edit-reviews`} value={review?.id} className="edit">Edit</NavLink>
+                                    <button hidden={review?.user_id !== user.id} className="delete" value={review?.id} onClick={handleReviewDelete} type="submit">Delete</button>
+                                </>
+                            }
+                        </div>
+                    ))}
+
+                    {user && !(product?.user_id === user?.id) &&
+                        <AddReviewForm productId={productId} />}                    
+                </div>
+
+                <div id="right-detail-div">
+                    <h2>Name: {product?.name}</h2>
+                    <p>Developer: {stateUsers[product?.user_id]?.username}</p>
+                    <p>Category: {category[product?.category_id - 1]?.name}</p>
+                    <p>Price: {`$${product?.price}`}</p>
+                    <p>Description: {product?.description}</p>
+                    {!(product?.user_id === user?.id) &&
+                        <div>
+                            <form onSubmit={handleSubmit}>
+                                <input
+                                    className='quantity-input'
+                                    type='number'
+                                    value={itemQuantity}
+                                    onChange={(e) => setItemQuantity(e.target.value)}></input>
+                                <button type='submit'>Add to cart</button>
+                            </form>
+                        </div>}
+                    <NavLink hidden={user?.id === product?.user_id ? false : true} to={`/products/${product?.id}/edit-product`} value={product?.id} className="edit">Edit</NavLink>
+                    <button hidden={user?.id === product?.user_id ? false : true} className="delete" value={product?.id} onClick={handleDelete} type="submit">Delete</button>
+                </div>
+               
+            </div>
         </div>
     );
 }

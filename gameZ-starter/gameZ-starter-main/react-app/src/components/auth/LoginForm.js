8b120b478c0f9c034c742c3f6487327cce947346
Eric-Cortez <ecrcortez@gmail.com>
2022-02-12 14:56:49 -0800

    form styleing

--- gameZ-starter/gameZ-starter-main/react-app/src/components/auth/LoginForm.js
+++ gameZ-starter/gameZ-starter-main/react-app/src/components/auth/LoginForm.js
@@ -4,6 +4,7 @@ import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
 import "./LoginForm.css"
 import Demo from './Demo';
+import "../../components/Forms/GlobalForm.css"
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -34,35 +35,46 @@ const LoginForm = () => {
 
   return (
     <div id="login-form-div">
-      <form className="login-form" onSubmit={onLogin}>
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <div>
-          <label htmlFor='email'>Email</label>
-          <input
-            name='email'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={updateEmail}
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            name='password'
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={updatePassword}
-          />
-          <button type='submit'>Login</button>
-        </div>
-      </form>
-      <Demo />
+      <div className="login-div-style" >
+        <form onSubmit={onLogin}>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <h2 id="form-h2">Login</h2>
+          <div className='input-div'>
+            <label
+            className='input-label required-field'
+            htmlFor='email'>Email </label>
+            <input
+              className='title-input'
+              name='email'
+              required
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div>
+            <label 
+            className='input-div'
+            htmlFor='password'>Password </label>
+            <input
+              required
+              className='title-input required-field'
+              name='password'
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={updatePassword}
+            />
+            <button id="login-btn" type='submit'>Login</button>
+          </div>
+        </form>
+        <Demo />
+      </div>
     </div>
   );
 };

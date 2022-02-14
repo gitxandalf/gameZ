3ba623bee2dfcec793bb5f8d70e1593caf6b5235
suhaylkhan <suhaylmkhan@gmail.com>
2022-02-13 18:28:04 -0600

    fixed react warnings in SearchResults component

--- gameZ-starter/gameZ-starter-main/react-app/src/components/SearchResults/index.js
+++ gameZ-starter/gameZ-starter-main/react-app/src/components/SearchResults/index.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
-import { getCategory, getCategories } from '../../store/category';
+import { Link } from 'react-router-dom';
+import { getCategories } from '../../store/category';
 import { getProducts } from '../../store/product';
 import { loadCart } from '../../store/shoppingCart';
 import "./SearchResults.css"
@@ -9,8 +9,6 @@ import "./SearchResults.css"
 function SearchResults({ products, categories }) {
     const dispatch = useDispatch()
 
-    const allCategories = useSelector(state => state?.category?.entries)
-    const allProducts = useSelector(state => state?.product?.entries)
     const allUsers = useSelector(state => state?.product?.usersEntries)
     const sessionUser = useSelector(state => state?.session?.user);
 
@@ -18,7 +16,7 @@ function SearchResults({ products, categories }) {
         dispatch(getCategories())
         dispatch(getProducts())
         if (sessionUser) dispatch(loadCart(sessionUser.id))
-    }, [dispatch])
+    }, [dispatch, sessionUser])
 
     return (
         <div>
@@ -28,7 +26,7 @@ function SearchResults({ products, categories }) {
                 </div>
                 <div className='product-links'>{products && products?.map((product) => (
                     <div id="each-product-category" key={product?.id}>
-                        <Link id="image-link-a" key={product?.id} to={`/products/${product?.id}`}><img key={product?.id} className='image-link' src={product?.image_url}></img></Link>
+                        <Link id="image-link-a" key={product?.id} to={`/products/${product?.id}`}><img key={product?.id} className='image-link' src={product?.image_url} alt='product' ></img></Link>
                         <div id="category-product-info">
                             <Link id="info-link-a" key={product?.id} to={`/products/${product?.id}`}>
                                 <p className="product-info-cat title-product-cat" key={product?.id}> Title: {product?.name}</p>

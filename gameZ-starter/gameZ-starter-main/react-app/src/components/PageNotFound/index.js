15ac5868c2b8293cec81ee4b360c1fffd0cc57b5
suhaylkhan <suhaylmkhan@gmail.com>
2022-02-13 18:25:55 -0600

    fixed react warnings in PageNotFound component

--- gameZ-starter/gameZ-starter-main/react-app/src/components/PageNotFound/index.js
+++ gameZ-starter/gameZ-starter-main/react-app/src/components/PageNotFound/index.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import "./PageNotFound.css"
 
 
 function PageNotFound() {
-   
+
 
     return (
         <div id="error-404-div">
